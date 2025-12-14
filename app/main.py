8d704da76d2c157e8abfec503cc49669c1f60348
Artem <artem90078@gmail.com>
2025-12-14 10:31:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,78 @@
-class Validator:
-    pass
+from abc import ABC, abstractmethod
 
 
-class Number:
-    pass
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self.protected_name = '_' + name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value):
+        self.validate(value)
+        setattr(instance, self.protected_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
 
 
-class OneOf:
-    pass
+class Number(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+
+        if not (self.min_value <= value <= self.max_value):
+            raise ValueError(
+                f"Quantity should not be less than {self.min_value} "
+                f"and greater than {self.max_value}."
+            )
+
+
+class OneOf(Validator):
+    def __init__(self, options):
+        # Store as set for efficient lookup
+        self.options = set(options)
+        # Store a string representation of the options as a tuple for deterministic error messages
+        self.options_str = str(tuple(options))
+
+    def validate(self, value):
+        if value not in self.options:
+            # Use self.options_str here to produce the required format ('ketchup', 'mayo', 'burger').
+            raise ValueError(
+                f"Expected {value} to be one of {self.options_str}."
+            )
 
 
 class BurgerRecipe:
-    pass
+    buns = Number(min_value=2, max_value=3)
+    cheese = Number(min_value=0, max_value=2)
+    tomatoes = Number(min_value=0, max_value=3)
+    cutlets = Number(min_value=1, max_value=3)
+    eggs = Number(min_value=0, max_value=2)
+    sauce = OneOf(options=("ketchup", "mayo", "burger"))
+
+    def __init__(self, buns, cheese, tomatoes, cutlets, eggs, sauce):
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
+
+    def __repr__(self):
+        return (
+            f"BurgerRecipe("
+            f"buns={self.buns}, "
+            f"cheese={self.cheese}, "
+            f"tomatoes={self.tomatoes}, "
+            f"cutlets={self.cutlets}, "
+            f"eggs={self.eggs}, "
+            f"sauce='{self.sauce}')"
+        )
